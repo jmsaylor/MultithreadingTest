6dadaecb170dc049e458a123c5cc312ad54c36c1
jmsaylor <johnmsaylor@gmail.com>
2021-01-17 22:56:47 -0500

    Initial Commit

---
+++ src/com/johnsaylor/Main.java
@@ -0,0 +1,18 @@
+package com.johnsaylor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SimpleBuffered.test();
+
+
+
+    }
+}
